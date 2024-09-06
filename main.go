2da29d4d98eb0e260015e11fe46abb2cd1197d23
LIMIT <guzman.brandon12@Live.com>
2024-09-06 17:58:56 -0600

    feat: rank data

--- main.go
+++ main.go
@@ -31,6 +31,15 @@ type GetPlayerIdentityFromTwitchId struct {
 	PlayerId string `json:"playerId"`
 }
 
+type GetPlayerRank struct {
+	PlayerId string `json:"playerId"`
+}
+
+type PlayerRank struct {
+	SoloRank int `json:"soloRank"`
+	TeamRank int `json:"teamRank"`
+}
+
 type MatchmakingData struct {
 	SequenceNumber int `json:"sequenceNumber"`
 	Response       struct {
@@ -79,36 +88,7 @@ func main() {
 	// JoinChannel := make(chan string)
 	TWITCH_KEY := os.Getenv("TWITCH_KEY")
 	client := twitch.NewClient("Santaigg", TWITCH_KEY)
-	// soloRanks := map[int]string{
-	// 	1:  "Bronze 1",
-	// 	2:  "Bronze 2",
-	// 	3:  "Bronze 3",
-	// 	4:  "Bronze 4",
-	// 	5:  "Silver 1",
-	// 	6:  "Silver 2",
-	// 	7:  "Silver 3",
-	// 	8:  "Silver 4",
-	// 	9:  "Gold 1",
-	// 	10: "Gold 2",
-	// 	11: "Gold 3",
-	// 	12: "Gold 4",
-	// 	13: "Platinum 1",
-	// 	14: "Platinum 2",
-	// 	15: "Platinum 3",
-	// 	16: "Platinum 4",
-	// 	17: "Emerald 1",
-	// 	18: "Emerald 2",
-	// 	19: "Emerald 3",
-	// 	20: "Emerald 4",
-	// 	21: "Ruby 1",
-	// 	22: "Ruby 2",
-	// 	23: "Ruby 3",
-	// 	24: "Ruby 4",
-	// 	25: "Diamond 1",
-	// 	26: "Diamond 2",
-	// 	27: "Diamond 3",
-	// 	28: "Diamond 4",
-	// 	29: "Champion"}
+
 	var lastCrewDump time.Time
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		fmt.Println(message.Channel + " :: " + message.User.DisplayName + "( " + message.User.ID + " )" + " : " + message.Message)
@@ -295,6 +275,125 @@ func main() {
 			}
 		}
 
+		if strings.Contains(message.Message, "!rank") {
+			var playerId string
+			if message.Channel == "ethos" {
+				playerId = "E27C1FD1-4EEB-483D-952D-A7C904869509"
+			}
+			if message.Channel == "truo" {
+				playerId = "8D02F2C0-69B8-4CEE-9656-2D0866B44E9B"
+			}
+			if message.Channel == "staycationtg" {
+				playerId = "F0CD9516-6DFB-4235-8E04-32D6B820754C"
+			}
+			if message.Channel == "bugzvii" {
+				playerId = "30C3E8E8-B5A4-4461-B77C-567B9B3C762D"
+			}
+			if message.Channel == "steazecs" {
+				playerId = "BCD9F729-DA28-4802-8CF6-DE831B852D62"
+			}
+			if message.Channel == "limitediq__" {
+				playerId = strings.Split(message.Message, ":")[1]
+				playerId = strings.TrimSpace(playerId)
+			}
+			if message.Channel == "moepork" {
+				playerId = "39F848C1-A9A5-42DF-81AA-033191455DAA"
+			}
+			if message.Channel == "relyks" {
+				playerId = "DC5D1993-5B94-4F0C-8F57-DB51B0DAE7F1"
+			}
+			if message.Channel == "shroud" {
+				playerId = "CE4C88F7-7D66-417F-A3F5-01D0F9F52B90"
+			}
+			if message.Channel == "iitztimmy" {
+				playerId = "1d36bff3-1ac5-422f-bb21-f6524e0b83a0"
+			}
+			if message.Channel == "pieman" {
+				playerId = "a666813a-5cc1-48ac-bcdf-ac937bda38bf"
+			}
+			if message.Channel == "omegatooyew" {
+				playerId = "8edc5a72-933c-412a-af09-51f587099e89"
+			}
+			if message.Channel == "shrood" {
+				playerId = "CE4C88F7-7D66-417F-A3F5-01D0F9F52B90"
+			}
+			respUrl := "https://collective-production.up.railway.app/getPlayerRankData/" + playerId
+			resp, err := http.Get(respUrl)
+			if err != nil {
+				log.Fatalf("Couldn't get player rank data for playerId: %s", playerId)
+			}
+
+			defer resp.Body.Close()
+			playerRankBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("Issue while reading body of resp: %s", err)
+				return
+			}
+			playerRankData := PlayerRank{}
+
+			playerRankDataUnmarshalErr := json.Unmarshal(playerRankBody, &playerRankData)
+			if playerRankDataUnmarshalErr != nil {
+				log.Fatalf("Issue while unmarshalling json response from player rank data endpoint. %s", err)
+			}
+
+			twitchMessage := fmt.Sprintf("[Current Solo Rank]: %s | [Highest Team Rank]: %s", getSoloRankFromRankNumber(playerRankData.SoloRank), getTeamRankFromRankNumber(playerRankData.TeamRank))
+			client.Reply(message.Channel, message.ID, twitchMessage)
+			return
+		}
+
+		if strings.Contains(message.Message, "!myrank") {
+			var playerId string
+			userUrl := "https://collective-production.up.railway.app/getPlayerIdentityFromTwitchId/" + message.User.ID
+			playerIdReq, playerIdReqErr := http.Get(userUrl)
+			if playerIdReqErr != nil {
+				log.Fatalf("Couldn't get player-id from twitch-id for: %s", message.User.Name)
+				return
+			}
+			defer playerIdReq.Body.Close()
+			// read body
+			playerIdReqBody, err := io.ReadAll(playerIdReq.Body)
+			if err != nil {
+				log.Fatalf("impossible to read all body of response: %s", err)
+				return
+			}
+
+			playerIdentity := GetPlayerIdentityFromTwitchId{}
+			playerIdReqUnmarshalErr := json.Unmarshal(playerIdReqBody, &playerIdentity)
+			if playerIdReqUnmarshalErr != nil {
+				log.Fatalf("Error while unmarshaling getPlayerIdentityFromTwitchId: %s", playerIdReqUnmarshalErr)
+				return
+			}
+			if playerIdentity.PlayerId != "ERROR" {
+				playerId = playerIdentity.PlayerId
+			} else {
+				client.Reply(message.Channel, message.ID, "You must link your Spectre Divide account to Twitch!")
+				return
+			}
+
+			respUrl := "https://collective-production.up.railway.app/getPlayerRankData/" + playerId
+			resp, err := http.Get(respUrl)
+			if err != nil {
+				log.Fatalf("Couldn't get player rank data for playerId: %s", playerId)
+			}
+
+			defer resp.Body.Close()
+			playerRankBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("Issue while reading body of resp: %s", err)
+				return
+			}
+			playerRankData := PlayerRank{}
+
+			playerRankDataUnmarshalErr := json.Unmarshal(playerRankBody, &playerRankData)
+			if playerRankDataUnmarshalErr != nil {
+				log.Fatalf("Issue while unmarshalling json response from player rank data endpoint. %s", err)
+			}
+
+			twitchMessage := fmt.Sprintf("[Current Solo Rank]: %s | [Highest Team Rank]: %s", getSoloRankFromRankNumber(playerRankData.SoloRank), getTeamRankFromRankNumber(playerRankData.TeamRank))
+			client.Reply(message.Channel, message.ID, twitchMessage)
+			return
+		}
+
 		if strings.Contains(message.Message, "!spectrestats") {
 			twitchMessage := fmt.Sprintf("Use !crewstats to get %s's crew stats & !mycrewstats to get your own crew stats. !spectrestats will be re-implemented when ranked drops on Sept 10th.", message.Channel)
 			client.Reply(message.Channel, message.ID, twitchMessage)
@@ -330,4 +429,80 @@ func main() {
 
 func hasTimePassed(lastSet time.Time, duration time.Duration) bool {
 	return time.Since(lastSet) >= duration
+}
+
+func getSoloRankFromRankNumber(rankNumber int) string {
+	soloRanks := map[int]string{
+		0:  "Unranked",
+		1:  "Bronze 1",
+		2:  "Bronze 2",
+		3:  "Bronze 3",
+		4:  "Bronze 4",
+		5:  "Silver 1",
+		6:  "Silver 2",
+		7:  "Silver 3",
+		8:  "Silver 4",
+		9:  "Gold 1",
+		10: "Gold 2",
+		11: "Gold 3",
+		12: "Gold 4",
+		13: "Platinum 1",
+		14: "Platinum 2",
+		15: "Platinum 3",
+		16: "Platinum 4",
+		17: "Emerald 1",
+		18: "Emerald 2",
+		19: "Emerald 3",
+		20: "Emerald 4",
+		21: "Ruby 1",
+		22: "Ruby 2",
+		23: "Ruby 3",
+		24: "Ruby 4",
+		25: "Diamond 1",
+		26: "Diamond 2",
+		27: "Diamond 3",
+		28: "Diamond 4",
+		29: "Champion"}
+
+	return soloRanks[rankNumber]
+}
+
+func getTeamRankFromRankNumber(rankNumber int) string {
+	teamRanks := map[int]string{
+		0:  "Unranked",
+		1:  "Undiscovered 1",
+		2:  "Undiscovered 2",
+		3:  "Undiscovered 3",
+		4:  "Undiscovered 4",
+		5:  "Prospect 1",
+		6:  "Prospect 2",
+		7:  "Prospect 3",
+		8:  "Prospect 4",
+		9:  "Talent 1",
+		10: "Talent 2",
+		11: "Talent 3",
+		12: "Talent 4",
+		13: "Professional 1",
+		14: "Professional 2",
+		15: "Professional 3",
+		16: "Professional 4",
+		17: "Elite 1",
+		18: "Elite 2",
+		19: "Elite 3",
+		20: "Elite 4",
+		21: "International 1",
+		22: "International 2",
+		23: "International 3",
+		24: "International 4",
+		25: "Superstar 1",
+		26: "Superstar 2",
+		27: "Superstar 3",
+		28: "Superstar 4",
+		29: "World Class 1",
+		30: "World Class 2",
+		31: "World Class 3",
+		32: "World Class 4",
+		33: "Champion"}
+
+	return teamRanks[rankNumber]
 }
