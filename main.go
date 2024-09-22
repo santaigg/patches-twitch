39ebca6ef46750965fd156138a2528cc496c9b03
LIMIT <guzman.brandon12@Live.com>
2024-09-22 00:54:52 -0600

    feat: add pqstreams

--- main.go
+++ main.go
@@ -343,7 +343,7 @@ func main() {
 		}
 	})
 
-	irc_client.Join("truo", "limitediq__", "staycationtg", "ethos", "bugzvii", "steazecs", "moepork", "shroud", "pieman", "shrood", "omegatooyew", "bixle", "equustv", "just9n", "enerbewow", "guccirushx", "k22g", "collinwth", "pr1ofps", "itsfjc")
+	irc_client.Join("truo", "limitediq__", "staycationtg", "ethos", "bugzvii", "steazecs", "moepork", "shroud", "pieman", "shrood", "omegatooyew", "bixle", "equustv", "just9n", "enerbewow", "guccirushx", "k22g", "collinwth", "pr1ofps", "itsfjc", "pqstreams")
 
 	app := fiber.New()
 
