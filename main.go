59218d7cf3d8d4d653bab6d4e115ca40182076ce
LIMIT <guzman.brandon12@Live.com>
2024-09-25 23:34:43 -0600

    feat: add pigeonr6

--- main.go
+++ main.go
@@ -343,7 +343,7 @@ func main() {
 		}
 	})
 
-	irc_client.Join("truo", "limitediq__", "staycationtg", "ethos", "bugzvii", "steazecs", "moepork", "shroud", "pieman", "shrood", "omegatooyew", "bixle", "equustv", "just9n", "enerbewow", "guccirushx", "k22g", "collinwth", "pr1ofps", "itsfjc", "pqstreams", "noctopusfps")
+	irc_client.Join("truo", "limitediq__", "staycationtg", "ethos", "bugzvii", "steazecs", "moepork", "shroud", "pieman", "shrood", "omegatooyew", "bixle", "equustv", "just9n", "enerbewow", "guccirushx", "k22g", "collinwth", "pr1ofps", "itsfjc", "pqstreams", "noctopusfps", "pigeonr6")
 
 	app := fiber.New()
 
